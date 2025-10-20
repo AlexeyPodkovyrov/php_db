e610b4daae6699e0748e0aa38569165d7546f78e
alexeypodkovyrov <apodkovyrov@mail.ru>
2025-10-20 13:58:39 +1100

    Изменил config.php

--- config.php
+++ config.php
@@ -4,10 +4,7 @@ return [
         'dsn' => 'mysql:host=localhost;dbname=taskapp;cahrset=utf8mb4',
         'user' => 'root',
         'pass' => '',
-        'options' => [
-            PDO::ATTR_ERMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSC,
-            PDO::ATTR_EMULATE_PREPARES => false
+        'options' => [           
         ]
     ],
     'storage' => [
